38816b7e1a909cc69bd2cf5d4b3cff0ee55d1280
Andrew Cho <ac0735@princeton.edu>
2025-03-28 02:39:24 -0400

    basic image upload functionality implemented

--- Frontend/src/components/Grid.jsx
+++ Frontend/src/components/Grid.jsx
@@ -12,16 +12,21 @@ function Grid({ posts = [], loading, error }) {
             <div className="post-title">
               <strong>{post.title}</strong>
             </div>
+            {post.image_url && (
+              <div className="post-image">
+                <img src={post.image_url} alt={post.title} />
+              </div>
+            )}
             <div className="post-content">
               <p>{post.content}</p>
             </div>
             <div className="post-footer">
-          <div className="post-tag">
-            <span>{post.tag ? post.tag : "No tag"}</span>
+              <div className="post-tag">
+                <span>{post.tag ? post.tag : "No tag"}</span>
+              </div>
+              <p className="post-date">Created at {post.date_created}</p>
+            </div>
           </div>
-          <p className="post-date">Created at {post.date_created}</p>
-        </div>
-      </div>
         ))
       ) : (
         <p>No items found.</p>
