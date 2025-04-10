e598e8aa7c5387251e24a189ab3f3184e65c878c
Henry Li <hl3547@princeton.edu>
2025-04-09 22:27:35 -0400

    admin

--- Backend/App.py
+++ Backend/App.py
@@ -137,8 +137,13 @@ def logout():
 def get_user():
     user = get_current_user()
     if user:
-        if user.email == 'hl3547@princeton.edu':
+        if user.email == 'hl3547@princeton.edu' :
             user.role = 'admin'
+        if user.email == 'brian.seo@@princeton.edu' :
+            user.role = 'admin'
+        if user.email == 'cho.andrew@princeton.edu' :
+            user.role = 'admin'
+
 
         return jsonify({
             'authenticated': True,
