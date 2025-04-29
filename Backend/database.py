35a57c95e8f9934b99add45e08bce18d69ed0db7
Andrew Cho <ac0735@princeton.edu>
2025-04-29 01:58:28 -0400

    deleted ENUM

--- Backend/database.py
+++ Backend/database.py
@@ -30,7 +30,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.name}>'
-    
+
 # Media Table
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key=True)
