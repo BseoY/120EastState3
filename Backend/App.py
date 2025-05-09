562fd590923d93a4c93c0ccd7265b63832dbcb72
Brian Seo <bs1207@princeton.edu>
2025-05-08 22:25:39 -0400

    app.py

--- Backend/App.py
+++ Backend/App.py
@@ -38,9 +38,9 @@ if not app.debug:
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
     ))
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.DEBUG)
     app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
+    app.logger.setLevel(logging.DEBUG)
     app.logger.info('120 East State backend startup')
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
